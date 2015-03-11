a476c6608cd0725f294d192661a56de427edf179
Claudio Di Ciccio <cdc08x@gmail.com>
2015-03-11 15:56:47 +0100

    Better integration with Declare and OpenXES libraries.

--- make-MINERful-distributable.sh
+++ make-MINERful-distributable.sh
@@ -6,6 +6,6 @@ jar cvfm "MINERful-v${VERSION_DATE}.jar" manifest.mf lib src -C ./bin .
 cp "MINERful-v${VERSION_DATE}.jar" "MINERful.jar"
 zip -r "MINERful-v${VERSION_DATE}" \
     MINERful.jar \
-    READ.me LICENSE licenses \
-    run*.sh test*.sh constraintsFunctions.cfg
+    README.mv LICENSE licenses \
+    run*.sh constraintsFunctions.cfg
 cp "MINERful-v${VERSION_DATE}.zip" "MINERful.zip"
